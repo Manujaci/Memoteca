45890a4dfc5033790bc565dd77f60dac31abd5f6
Manoel Jacinto <manujacidev@gmail.com>
2026-08-27 01:31:54 -0300

    adicionando post method

--- js/api.js
+++ js/api.js
@@ -7,7 +7,25 @@ const api = {
             alert('Erro ao buscar pensamentos.')
             throw error
         }
+    },
+
+    async salvarPensamento(pensamento) {
+        try {
+            const response = await fetch('http://localhost:3000/pensamentos', {
+                method: "POST",
+                Headers: {
+                    "content-type": "aplication/json"
+                },
+                body: JSON.stringify(pensamento)
+            })
+            return await response.json()
+        } catch {
+            alert('Erro ao enviar pensamentos.')
+            throw error
+        }
     }
+
+  
 }
 
 export default api
--- js/main.js
+++ js/main.js
@@ -1,5 +1,35 @@
 import ui from "./ui.js"
+import api from "./api.js"
 
 document.addEventListener('DOMContentLoaded', ()=>{
     ui.renderizarPensamentos()
+
+    const formularioPensamento = document.getElementById("pensamento-form")
+    formularioPensamento.addEventListener("submit", manipularSubmissaoFormulario)
+
 })
+
+async function manipularSubmissaoFormulario(event) {
+    event.preventDefault()
+
+    const id =  document.getElementById("pensamento-id").value
+    const conteudo = document.getElementById("pensamento-conteudo").value
+    const autoria = document.getElementById("pensamento-autoria").value
+
+    try {
+        await api.salvarPensamento({conteudo, autoria})
+        ui.renderizarPensamentos()
+    } catch {
+        alert("Erro ao enviar formulário")
+    }
+
+}
+
+    const cancelar = document.getElementById("botao-cancelar")
+    cancelar.addEventListener("click", () => {
+        let conteudo = document.getElementById("pensamento-conteudo")
+        let autoria = document.getElementById("pensamento-autoria")
+
+        conteudo.value = ""
+        autoria.value = ""
+    })
