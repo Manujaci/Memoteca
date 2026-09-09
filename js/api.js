17dc2a4361e46e4be93806f296f2840464f73e75
Manoel Jacinto <manujacidev@gmail.com>
2026-09-09 01:34:09 -0300

    adcionando ferramenta de editar

--- js/api.js
+++ js/api.js
@@ -20,10 +20,38 @@ const api = {
             })
             return await response.json()
         } catch {
-            alert('Erro ao enviar pensamentos.')
+            alert('Erro ao salvar pensamento.')
+            throw error
+        }
+    },
+
+    async buscarPensamentoPorId(id) {
+        try {
+            const response = await fetch(`http://localhost:3000/pensamentos/${id}`)
+            return await response.json()
+        } catch {
+            alert('Erro ao buscar pensamento.')
+            throw error
+        }
+    },
+
+       async editarPensamento(pensamento) {
+        try {
+            const response = await fetch(`http://localhost:3000/pensamentos/${pensamento.id}`, {
+                method: "PUT",
+                Headers: {
+                    "content-type": "aplication/json"
+                },
+                body: JSON.stringify(pensamento)
+            })
+            return await response.json()
+        } catch {
+            alert('Erro ao editar pensamento.')
             throw error
         }
     }
+
+ 
 
   
 }
